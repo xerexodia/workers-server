97c043815de15fd627b6578895aadfbbb271d334
xerexodia <ibrahimfarhat2018@gmail.com>
2023-04-26 18:25:16 +0200

    creating post controller

---
+++ src/Controllers/post/postController.ts
@@ -0,0 +1,101 @@
+import {
+  Controller,
+  Delete,
+  FormField,
+  Get,
+  Patch,
+  Path,
+  Post,
+  Request,
+  Res,
+  Route,
+  SuccessResponse,
+  TsoaResponse,
+  UploadedFile,
+} from 'tsoa';
+import express from 'express';
+import fs from 'fs';
+import { hashIt, validateEmail } from '../../utils/helpers';
+import { IWorker } from '../../Models/worker';
+import { WorkersServices } from '../../Services/workerServices';
+import { IPost } from '../../Models/post';
+import { PostServices } from '../../Services/postServices';
+
+// post controller
+@Route('post')
+export class PostController extends Controller {
+  // add a post
+  @SuccessResponse('201', 'created successfully') // Custom success response
+  @Post()
+  public async createPost(
+    @FormField() title: string,
+    @FormField() adresse: string,
+    @FormField() description: string,
+    @FormField() clientId: string,
+    @UploadedFile() photo: Express.Multer.File,
+    @Request() req: express.Request,
+  ): Promise<IPost> {
+    const b64 = Buffer.from(photo?.buffer as any).toString('base64');
+    const buffer = Buffer.from(b64, 'base64');
+    const imagename = `${Date.now()}.jpeg`;
+    fs.writeFileSync(`./src/uploads/${imagename}`, buffer);
+    const basePath = `${req?.protocol}://${req?.get('host')}/src/uploads/`;
+    const post = await new PostServices().create({
+      title,
+      adresse,
+      description,
+      clientId,
+      photo: `${basePath}${imagename}`,
+    });
+    return post;
+  }
+  //   get all posts
+  @SuccessResponse('201', 'fetched successfully') // Custom success response
+  @Get()
+  public async getPosts(): Promise<IPost[]> {
+    const posts = await new PostServices().getAll();
+    return posts;
+  }
+
+  // get post by id
+  @SuccessResponse('201', 'fetched successfully') // Custom success response
+  @Get('{postId}')
+  public async getPostById(@Path() postId: string): Promise<IPost> {
+    const posts = await new PostServices().getById(postId);
+    return posts;
+  }
+
+  // update post
+  @SuccessResponse('201', 'updated successfully') // Custom success response
+  @Patch('{postId}')
+  public async updatePost(
+    @Request() req: express.Request,
+    @Path() postId: string,
+    @FormField() title?: string,
+    @FormField() adresse?: string,
+    @FormField() description?: string,
+    @FormField() clientId?: string,
+    @UploadedFile() photo?: Express.Multer.File,
+  ): Promise<IPost> {
+    const b64 = Buffer.from(photo?.buffer as any).toString('base64');
+    const buffer = Buffer.from(b64, 'base64');
+    const imagename = `${Date.now()}.jpeg`;
+    fs.writeFileSync(`./src/uploads/${imagename}`, buffer);
+    const basePath = `${req?.protocol}://${req?.get('host')}/src/uploads/`;
+    const post = await new PostServices().update(postId, {
+      title,
+      adresse,
+      description,
+      clientId,
+      photo: `${basePath}${imagename}`,
+    });
+    return post!;
+  }
+
+  //delete a post
+  @SuccessResponse('201', 'deleted successfully') // Custom success response
+  @Delete('{postId}')
+  public async delete(@Path() postId: string): Promise<void> {
+    await new PostServices().delete(postId);
+  }
+}

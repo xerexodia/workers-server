3aea8d5e632ceacbed1e5e894af859e005cd5e09
xerexodia <ibrahimfarhat2018@gmail.com>
2023-04-26 18:24:38 +0200

    creating client register controller

--- src/Controllers/auth/registerClientController.ts
+++ src/Controllers/auth/registerClientController.ts
@@ -1,35 +1,46 @@
 import multer from 'multer';
 import { IClient } from '../../Models/client';
 import { ClientsService } from '../../Services/clientsService';
-import { Controller, FormField, Get, Path, Post, Request, Route, SuccessResponse, UploadedFile } from 'tsoa';
+import {
+  Controller,
+  FormField,
+  Get,
+  Path,
+  Post,
+  Request,
+  Res,
+  Route,
+  SuccessResponse,
+  TsoaResponse,
+  UploadedFile,
+} from 'tsoa';
 import express from 'express';
 import fs from 'fs';
-import { hashIt } from '../../utils/helpers';
+import { hashIt, validateEmail } from '../../utils/helpers';
 
-@Route('clients')
-export class ClientsController extends Controller {
-  // get client by id
-  @Get('{id}')
-  public async getUser(@Path() id: string): Promise<IClient | null> {
-    return new ClientsService().get(id);
-  }
-
+@Route('register/clients')
+export class ClientsRegisterController extends Controller {
   // post client controller
   @SuccessResponse('201', 'Bienvenue') // Custom success response
   @Post()
   public async createClient(
-    @FormField() nom?: string,
-    @FormField() prenom?: string,
-    @FormField() email?: string,
-    @FormField() password?: string,
-    @FormField() adresse?: string,
-    @UploadedFile() photo?: Express.Multer.File,
-    @Request() req?: express.Request,
-  ): Promise<IClient | null | undefined> {
-    try {
+    @Res() notFoundResponse: TsoaResponse<401, { msg: string }>,
+    @FormField() nom: string,
+    @FormField() prenom: string,
+    @FormField() email: string,
+    @FormField() password: string,
+    @FormField() adresse: string,
+    @Request() req: express.Request,
+    @UploadedFile() photo: Express.Multer.File,
+  ): Promise<IClient | undefined | string> {
+    const client = await new ClientsService().findClient(email!);
+    if (client) {
+      return notFoundResponse(401, { msg: 'you have already an account' });
+    } else {
+      if (!validateEmail(email)) return notFoundResponse(401, { msg: 'wrong mail' });
+      if (password.length <= 8) return notFoundResponse(401, { msg: 'password must be at least 8 characters' });
       const b64 = Buffer.from(photo?.buffer as any).toString('base64');
       const buffer = Buffer.from(b64, 'base64');
-      console.log(buffer);
       const imagename = `${Date.now()}.jpeg`;
       fs.writeFileSync(`./src/uploads/${imagename}`, buffer);
       const basePath = `${req?.protocol}://${req?.get('host')}/src/uploads/`;
@@ -43,9 +54,8 @@ export class ClientsController extends Controller {
         adresse,
         photo: `${basePath}${imagename}`,
       });
-    } catch (error) {
-      console.log(error);
     }
+
     return;
   }
 }

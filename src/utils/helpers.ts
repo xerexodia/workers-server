2a63a912ab85e0dcb7c0edbb5cfccd3de715ce83
xerexodia <ibrahimfarhat2018@gmail.com>
2023-04-26 18:26:58 +0200

    creating helper fcts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,6 +3,13 @@ const bcrypt = require('bcrypt');
 export function hashIt(pwd: string | undefined) {
   return bcrypt.hashSync(pwd, 10) as string;
 }
-export async function compareIt(pwd: string | undefined, hashedPwd: string | undefined) {
-  return (await bcrypt.compareSync(pwd, hashedPwd)) as boolean;
+export function compareIt(pwd: string | undefined, hashedPwd: string | undefined): boolean {
+  const compare = bcrypt.compareSync(pwd, hashedPwd);
+  return compare;
 }
+
+export const validateEmail = (email: string) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  );
+};
